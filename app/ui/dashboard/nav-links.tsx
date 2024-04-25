2336dad97ee796dadde7554d966177891593130d
ssohyeon-jang <kate@10k1m.com>
2024-04-25 09:51:27 +0900

    수정

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// Map of links to display in the side navigation.asdfasf
+// Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
